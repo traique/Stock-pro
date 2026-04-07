bebdaafe16dc7b22cc464a93f263c924ea0493b9
traique <95959617+traique@users.noreply.github.com>
2026-04-07 22:23:20 +0700

    Create tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1E40AF",
+        success: "#22C55E",
+        danger: "#EF4444",
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
